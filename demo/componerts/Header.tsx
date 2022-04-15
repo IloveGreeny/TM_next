fe8f828178bbb316d20ea0149356f39119dc5bba
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-04-15 22:35:05 +0400

    Update Header.tsx pt.3

--- demo/componerts/Header.tsx
+++ demo/componerts/Header.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className="flex justify-between p-5 bg-blue-600">
+            <div className="flex items-center space-x-5">
+                <Link href="/">
+                    <img className="w-44 object-contain cursor-pointer" src="" alt=""/>
+                </Link>
+            </div>
+            <div className="flex items-center space-x-5 text-white">
+                <Link href="/">
+                    <h3 className="cursor-pointer text-white">Home</h3>
+                </Link>
+                <Link href="card">
+                    <h3 className="cursor-pointer text-white">Cards</h3>
+                </Link>
+                <h3 className="border px-4 py-1 rounded-full text-white">Get Started</h3>
+            </div>
+        </header>
     )
 }
