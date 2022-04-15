1a56f8fc20d5c372dbb4eba06c52eb73dd98880e
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-04-15 20:58:52 +0400

    Delete Index.tsx

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,19 +1,1 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from "../componerts/Header";
 
-function Home () {
-  return (
-    <div className="">
-      <Head>
-        <title>WebPage</title>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-
-        <Header />
-    </div>
-  )
-}
-
-export default Home
