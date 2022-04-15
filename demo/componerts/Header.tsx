f0dba789ea68e57f33920779c0e661cc16a559e2
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-04-15 20:57:44 +0400

    Delete Header.tsx

--- demo/componerts/Header.tsx
+++ demo/componerts/Header.tsx
@@ -1,19 +1,1 @@
-import Link from "next/link";
 
-
-export default function Header() {
-    return(
-        <header>
-            <div>
-                <Link href="/">
-                    <img
-                        src="https://i.scdn.co/image/ab6761610000e5eb05534cdc751e86a9c4fef5c9"
-                        alt=""/>
-                </Link>
-            </div>
-            <div>
-
-            </div>
-        </header>
-    )
-}
