f6ddd922af90688bc5c7a95570f2e06bcbcf4860
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-04-15 22:35:41 +0400

    Update index.tsx pt.3

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,33 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Card from "./card";
 import Link from "next/link";
+import Header from "../componets/Header";
 
 
 
-
-
-
-function Home() {
-  return (
-    <header className="flex justify-between p-5">
-        <div className="flex items-center space-x-5">
-            <Link href="/">
-                <img className="w-44 object-contain cursor-pointer" src="" alt=""/>
-            </Link>
-        </div>
-        <div className="flex items-center space-x-5 text-blue-600">
-            <Link href="/">
-                <h3 className="cursor-pointer">Home</h3>
-            </Link>
+export default function Home() {
+    return(
+        <div>
+           <Header/>
             <Link href="card">
-                <h3 className="cursor-pointer">Cards</h3>
+                <h1 className="cursor-pointer ">KLA/</h1>
             </Link>
-            <h3 className="border px-4 py-1 rounded-full border-blue-600">Get Started</h3>
         </div>
-    </header>
-  )
+    )
 }
-
-export default Home
