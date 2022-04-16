ce7c3aa8af8dd5814f5024996c86a80eee19547e
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-04-16 19:15:52 +0400

    Update index.tsx pt.4

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,6 +1,8 @@
 import Card from "./card";
 import Link from "next/link";
-import Header from "../componets/Header";
+import Header from "./componets/Header";
+import Body from "./componets/Body";
+
 
 
 
@@ -8,9 +10,8 @@ export default function Home() {
     return(
         <div>
            <Header/>
-            <Link href="card">
-                <h1 className="cursor-pointer ">KLA/</h1>
-            </Link>
+            <Body/>
         </div>
     )
 }
+
