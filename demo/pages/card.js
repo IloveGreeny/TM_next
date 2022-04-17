db138839e782bb31c50a740ca702728fd921e765
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-04-17 14:16:51 +0400

    Update Card.js pt.2

--- demo/pages/card.js
+++ demo/pages/card.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
+
 export default function Card() {
     return(
         <div>
-            Cards
+            <Link href="/">
+                <img
+                    src="https://images.unsplash.com/photo-1597200381847-30ec200eeb9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"
+                    alt=""
+                    className="cursor-pointer"/>
+            </Link>
         </div>
     )
 }
